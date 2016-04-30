67107067e4570d6dccda6a2905fc4320e3a53226
jacky6016 <chlin6016@gmail.com>
2016-04-30 12:58:49 +0800

    Version 0.1(draft)

--- lab3/lab3.cu
+++ lab3/lab3.cu
@@ -27,6 +27,30 @@ __global__ void SimpleClone(
 	}
 }
 
+__global__ void CalculateFixed(
+	const float *background, 
+	const float *target, 
+	const float *mask, 
+	 float *fixed, 
+	const int wb, const int hb, 
+	const int wt, const int ht, 
+	const int oy, const int ox)
+{
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+	// boundary	condition: (1) boundary of mask (2) boundary of background 
+	float Nb, Wb, Sb, Eb;
+	if(mask[idx] == 0.0f)
+//		fixed[idx] = ;
+	else
+//		fixed[idx] = 4*target[idx] - (target[idx-wt] + target[idx-1] + target[idx+1] + target[idx+wt])
+}
+
+__global__ void PoissonImageCloningIteration(float *fixed, const float *mask, float *buf1, float *buf2, const int wt, const int ht)
+{
+	// formula: Cb' = 1/4
+}
+
 void PoissonImageCloning(
 	const float *background,
 	const float *target,
@@ -36,9 +60,47 @@ void PoissonImageCloning(
 	const int oy, const int ox
 )
 {
+
+/* No addtional editing: paste the target to background, through mask.
 	cudaMemcpy(output, background, wb*hb*sizeof(float)*3, cudaMemcpyDeviceToDevice);
 	SimpleClone<<<dim3(CeilDiv(wt,32), CeilDiv(ht,16)), dim3(32,16)>>>(
 		background, target, mask, output,
 		wb, hb, wt, ht, oy, ox
 	);
+*/
+ 
+	// set up
+	float *fixed, *buf1, *buf2;
+	cudaMalloc(&fixed, 3*wt*ht*sizeof(float));
+	cudaMalloc(&buf1, 3*wt*ht*sizeof(float));
+	cudaMalloc(&buf2, 3*wt*ht*sizeof(float));
+
+	// initialize the iteration
+	dim3 gdim(CeilDiv(wt, 32), CeilDiv(ht, 16)), bdim(32, 16);
+	CalculateFixed<<<gdim, bdim>>>(
+		background, target, mask, fixed,
+		wb, hb, wt, ht, oy, ox
+	);
+	cudaMemcpy(buf1, target, sizeof(float)*3*wt*ht, cudaMemcpyDeviceToDevice);
+	
+	// iterate
+	for (int i = 0; i < 10000; ++i) {
+		PoissonImageCloningIteration<<<gdim, bdim>>>(
+			fixed, mask, buf1, buf2, wt, ht
+		);
+		PoissonImageCloningIteration<<<gdim, bdim>>>(
+			fixed, mask, buf2, buf1, wt, ht
+		);
+	}
+
+	// copy the image back
+	cudaMemcpy(output, background, wb*hb*sizeof(float)*3, cudaMemcpyDeviceToDevice);
+	SimpleClone<<<gdim, bdim>>>(
+		background, buf1, mask, output,
+		wb, hb, wt, ht, oy, ox
+	);
+	// clean up
+	cudaFree(fixed);
+	cudaFree(buf1);
+	cudaFree(buf2);
 }
